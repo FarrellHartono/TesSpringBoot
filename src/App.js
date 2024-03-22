4de213989301b0d8d8bd9ab87aaf9caab465223a
FarrellHartono <farrellhartono@gmail.com>
2024-03-22 11:24:10 +0700

    ngePush React

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./layout/Navbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddUser from "./users/AddUser";
+import EditUser from "./users/EditUser";
+import ViewUser from "./users/ViewUser";
 
 function App() {
   return (
@@ -14,7 +16,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/home" element={<Home />} />
-          <Route exact path="/addUser" element={<AddUser />}/>
+          <Route exact path="/addUser" element={<AddUser />} />
+          <Route exact path="/viewuser/:id" element={<ViewUser />} />
+          <Route exact path="/edituser/:id" element={<EditUser />} />
         </Routes>
       </Router>
     </div>
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -8,14 +8,16 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" to={"/home"}>
             Full Stack Application
-          </a>
+          </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <Link className="btn btn-outline-light" to="/addUser">Add User</Link>
+          <Link className="btn btn-outline-light" to="/addUser">
+            Add User
+          </Link>
         </div>
       </nav>
     </div>
